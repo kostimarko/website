2ae3532f85cc2764d15f71a08b8ec099614f0a02
kostimarko <kostimarko@gmail.com>
2020-08-19 23:36:43 -0500

    removed timeout

--- src/components/transition/transition.js
+++ src/components/transition/transition.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import posed, { PoseGroup } from 'react-pose';
-import { timeout } from 'constants/transition';
 
 class Transition extends PureComponent {
   render() {
